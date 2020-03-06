02ae19ff95f7022d66aac153906bbebd71bb9af3
Rishi Khan <rishi.k@ecobee.com>
2020-03-06 15:12:39 -0500

    Removed local file system name from build.sh

--- support/source/ios/build.sh
+++ support/source/ios/build.sh
@@ -39,7 +39,7 @@ if [ -d "./build" ]; then
 rm -rf "./build"
 fi
 
-echo "Mockifer: iOS Framework: Build complete! Find '${FRAMEWORK_NAME}.framework' in the 'Automation/MockiferEcobee/FrameworkBuild' folder. See the 'sample-ios' project to find out how to integrate the framework."
+echo "Mockifer: iOS Framework: Build complete! Find '${FRAMEWORK_NAME}.framework' in the 'products/libs/ios' folder. See the 'sample-ios' project to find out how to integrate the framework."
 echo ''
 echo '======================================================='
 echo 'MOCKIFER: iOS FRAMEWORK BUILD FINISHED'
